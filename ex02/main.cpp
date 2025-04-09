1ec43070b9f1283b4c77698e6d034eac2ef23afb
Quentin Liso <qliso@paul-f4Ar2s5.clusters.42paris.fr>
2025-04-09 17:59:06 +0200

    aaa

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,14 +6,14 @@
 /*   By: qliso <qliso@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 12:11:20 by qliso             #+#    #+#             */
-/*   Updated: 2025/04/07 15:30:52 by qliso            ###   ########.fr       */
+/*   Updated: 2025/04/07 15:37:25 by qliso            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Array.hpp"
 
-int main ( void )
+void    test1( void )
 {
     Array<int> *arr = new Array<int>(3);    
     try
@@ -45,5 +45,46 @@ int main ( void )
     std::cout << "Arrigato[1] : " << arrigato[1] << std::endl;
 
     delete arr;
+}
+
+void    test2( void )
+{
+    Array<char> *arr = new Array<char>(17);
+    try
+    {
+        (*arr)[14] = 'u';
+        std::cout << "Arr[14] : " << (*arr)[14] << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    Array<char> array(4);
+    try
+    {
+        array[2] = 'z';
+        std::cout << "Array[2] : " << array[2] << std::endl;
+        array[5] = 'w';
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    Array<char> arrigato(array);
+    std::cout << "Arrigato[2] : " << arrigato[2] << std::endl;
+    arrigato[2] = 'a';
+    std::cout << "Array[2] : " << array[2] << std::endl;
+    std::cout << "Arrigato[2] : " << arrigato[2] << std::endl;
+
+    delete arr;
+}
+
+
+int main ( void )
+{
+    test1();
+    test2();
     return ( 0 );
 }
